89330d81402be01aecbb3cb02a8990ddf1bc2b57
Leornadia <leornadiajose@gmail.com>
2023-10-06 13:53:31 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,16 @@
  */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
+putchar('0');
+putchar('1' + 1);
+putchar('1' + 2);
+putchar('1' + 3);
+putchar('1' + 4);
+putchar('1' + 5);
+putchar('1' + 6);
+putchar('1' + 7);
+putchar('1' + 8);
+putchar('1' + 9);
+putchar('\n');
 return (0);
 }
