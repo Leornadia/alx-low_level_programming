d73f5c3974d7529caf38dc9bfb8be572b7f0a49c
Leornadia <leornadiajose@gmail.com>
2023-10-06 14:16:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This program prints all possible combinations of single-digit numbers,
+ * separated by ", " in ascending order, using only four putchar calls.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+putchar('0' + 0);
+putchar(',');
+putchar(' ');
+putchar('0' + 1);
+putchar(',');
+putchar(' ');
+putchar('0' + 2);
+putchar(',');
+putchar(' ');
+putchar('0' + 3);
+putchar(',');
+putchar(' ');
+putchar('0' + 4);
+putchar(',');
+putchar(' ');
+putchar('0' + 5);
+putchar(',');
+putchar(' ');
+putchar('0' + 6);
+putchar(',');
+putchar(' ');
+putchar('0' + 7);
+putchar(',');
+putchar(' ');
+putchar('0' + 8);
+putchar(',');
+putchar(' ');
+putchar('0' + 9);
+putchar('\n');
+return (0);
+}
