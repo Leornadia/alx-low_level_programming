1b7cbf1c710452483a0ad398593e18434e228b0b
Leornadia <leornadiajose@gmail.com>
2023-10-10 09:39:19 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* Function prototype */
-void print_alphabet(void);
-#endif /* MAIN_H */
 #include "main.h"
 /**
  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
