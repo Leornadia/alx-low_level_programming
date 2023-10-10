cfd5a32fb174b9ec912cffdb58efdc9cdeb93c5a
Leornadia <leornadiajose@gmail.com>
2023-10-10 13:10:07 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,13 @@ for (j = 0; j <= 9; j++)
 product = i * j;
 if (j == 0)
 _putchar('0');
-else if (product < 10)
+else
+{
+_putchar(',');
 _putchar(' ');
+if (product < 10)
+_putchar(' ');
+}
 if (product < 10)
 _putchar('0' + product);
 else
@@ -21,14 +26,7 @@ else
 _putchar('0' + (product / 10));
 _putchar('0' + (product % 10));
 }
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
 }
-else
 _putchar('\n');
 }
 }
-}
-
