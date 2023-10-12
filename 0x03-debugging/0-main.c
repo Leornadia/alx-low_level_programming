5e5c409639e1887dc50ca71303d238aa77853e61
Leornadia <leornadiajose@gmail.com>
2023-10-12 09:31:02 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
  * main - tests the positive_or_negative function with a case of 0
  * Return: Always 0
  */
 int main(void)
 {
-    int i;
-
-    i = 0;
-    positive_or_negative(i);
-
-    return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
-
