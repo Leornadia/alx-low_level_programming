0b8a188c94b9443e70150135bb4a3c85702b9fc3
Leornadia <leornadiajose@gmail.com>
2023-10-27 11:52:29 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
  *
  * Return: Always returns 0 to indicate success.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
