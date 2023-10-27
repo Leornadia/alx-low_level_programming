8714e38f91ba160a8b35b52654ea3d6ca4f88553
Leornadia <leornadiajose@gmail.com>
2023-10-27 10:48:47 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void printProgramName(char *programName);
+
+#endif /* MAIN_H */
+
