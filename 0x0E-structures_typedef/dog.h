4e06a85f1e3de4e76555a4d3fdac9040b47f86fa
Leornadia <leornadiajose@gmail.com>
2023-11-06 09:56:52 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,5 @@ char *name;
 float age;
 char *owner;
 } dog;
+typedef dog dog_t;
 #endif
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,9 +2,18 @@
 #define MAIN_H
 
 #include "dog.h"
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog;
+
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 struct dog *new_dog(char *name, float age, char *owner);
 void free_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif/*MAIN_H*/
