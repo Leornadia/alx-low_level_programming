b95f88a68e0e4c64e5a9dd68f1d83fabb8713e91
Leornadia <leornadiajose@gmail.com>
2023-11-07 02:24:45 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 /**
  * new_dog - Creates a new dog and stores a copy of name and owner.
  * @name: The name of the dog.
@@ -11,24 +11,25 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog = malloc(sizeof(dog_t));
+dog_t *new_dog;
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 {
 return (NULL);
 }
-new_dog->name = my_strdup(name);
-if (new_dog->name == NULL)
+new_dog->name = strdup(name);
+new_dog->owner = strdup(owner);
+if (new_dog->name == NULL || new_dog->owner == NULL)
 {
+free(new_dog->name);
+free(new_dog->owner);
 free(new_dog);
 return (NULL);
 }
 new_dog->age = age;
-new_dog->owner = my_strdup(owner);
-if (new_dog->owner == NULL)
-{
-free(new_dog->name);
-free(new_dog);
-return (NULL);
-}
 return (new_dog);
 }
