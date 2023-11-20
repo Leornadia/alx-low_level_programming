bb019890597a0b8ed8307de10823396256c05186
Leornadia <leornadiajose@gmail.com>
2023-11-20 09:48:19 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * free_listint2 - Frees a listint_t list and sets the head to NULL.
- * @head: Pointer to a pointer to the head of the list.
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to a pointer to the head of the list
+ * The function sets the head to NULL
  */
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
-while (*head != NULL)
+listint_t *tmp;
+while (*head)
 {
-temp = *head;
-*head = (*head)->next;
-free(temp);
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
 }
-*head = NULL;
+head = NULL;
 }
