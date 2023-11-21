a2778f89a6d5685bbb1bf7cf3bbfa9e86f6e60b9
Leornadia <leornadiajose@gmail.com>
2023-11-21 10:45:19 -0800

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,28 +6,3 @@
  * @head: pointer to the head of the list
  * Return: the number of nodes in the list
  */
-size_t print_listint_safe(const listint_t *head)
-{
-size_t count = 0;
-const listint_t *slow, *fast;
-if (head == NULL)
-return (0);
-slow = head;
-fast = head->next;
-while (slow != NULL)
-{
-printf("[%p] %d\n", (void *)slow, slow->n);
-count++;
-if (slow == fast)
-{
-printf("-> [%p] %d\n", (void *)fast, fast->n);
-break;
-}
-slow = slow->next;
-if (fast != NULL && fast->next != NULL)
-fast = fast->next->next;
-else
-fast = NULL;
-}
-return (count);
-}
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,35 +1,52 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "lists.h"
 /**
- * free_listint_safe - frees a listint_t list that may contain a loop
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t count = 0;
+const listint_t *current;
+if (!head)
+return (0);
+current = head;
+while (current)
+{
+printf("[%p] %d\n", (void *)current, current->n);
+if (current <= current->next)
+{
+printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+break;
+}
+current = current->next;
+count++;
+}
+return (count);
+}
+/**
+ * free_listint_safe - frees a listint_t list safely
  * @h: pointer to the head of the list
  * Return: the size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
 size_t count = 0;
-listint_t *temp, *slow, *fast;
-if (h == NULL || *h == NULL)
+listint_t *current, *tmp;
+if (!h || !*h)
 return (0);
-slow = *h;
-fast = (*h)->next;
-while (slow != NULL)
+current = *h;
+while (current)
 {
-temp = slow;
-slow = slow->next;
+printf("[%p] %d\n", (void *)current, current->n);
+tmp = current;
+current = current->next;
+free(tmp);
 count++;
-free(temp);
-if (slow == fast)
-{
-*h = NULL;
-return (count);
-}
-if (fast != NULL && fast->next != NULL)
-fast = fast->next->next;
-else
-fast = NULL;
+if (tmp <= current)
+break;
 }
 *h = NULL;
 return (count);
